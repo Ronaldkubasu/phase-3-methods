8cc6c24ce66830a59804679715411ce01b8f8a86
Ronaldkubasu <ronald.kubasu@student.moringaschool.com>
2022-10-18 12:57:50 +0300

    Completed Assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,53 @@
 # Your code here!
+
+# def greet_programmer
+#      puts "Hello,programmer!"
+# end
+
+# def greet(name = "Nureen!")
+#      puts "Hello, #{name}!"
+# end
+
+# greet
+
+# def greet_with_default(name = "programmer!")
+#     puts "Hello, #{name}!"
+# end
+# greet_with_default
+# greet_with_default "Sunny!"
+
+# def add(num1, num2)
+#     return num1 + num2
+# end
+
+
+# def halve num
+#     if num.class != integer
+#         return nil
+#       end
+#     return (num/2)
+# end
+
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+def greet param
+    puts "Hello, #{param}!"
+end
+
+def greet_with_default param='programmer'
+    puts "Hello, #{param}!"
+end
+
+def add num1,num2 
+    return num1+num2
+end
+
+def halve param
+    if param.class!=Integer
+        return nil
+    end
+    return (param/2)
+end
+
